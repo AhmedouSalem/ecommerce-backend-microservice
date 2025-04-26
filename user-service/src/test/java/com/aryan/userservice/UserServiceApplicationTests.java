639c05e832a6c1cb5bdc5cb47f678dc8e8139bbb
AhmedouSalem <ahmedou8salem@gmail.com>
2025-04-26 15:04:53 +0200

    build and tests

--- user-service/src/test/java/com/aryan/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/aryan/userservice/UserServiceApplicationTests.java
@@ -6,7 +6,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@TestPropertySource(properties = "ecom.token=test-token")
+@TestPropertySource(properties = {
+        "ecom.token=test-token",
+        "spring.config.import=optional:classpath:/application.properties"
+})
 @ComponentScan(basePackages = "com.aryan.userservice")
 class UserServiceApplicationTests {
 
