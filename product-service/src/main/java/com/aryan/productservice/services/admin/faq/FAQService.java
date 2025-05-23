8e225e61e491770189baace051f2802c7c3edc35
AhmedouSalem <ahmedou8salem@gmail.com>
2025-05-23 21:32:19 +0200

    Branche user-service : suppression des autres modules, conservation des fichiers nécessaires

--- product-service/src/main/java/com/aryan/productservice/services/admin/faq/FAQService.java
+++
@@ -1,8 +0,0 @@
-package com.aryan.productservice.services.admin.faq;
-
-
-import com.aryan.productservice.dto.FAQDto;
-
-public interface FAQService {
-    FAQDto postFAQ(Long productId, FAQDto faqDto);
-}
